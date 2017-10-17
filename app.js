be3be2a6085fb054781bc8e32bb6005517b87473
Sarah Ann Garcia <garciatres@gmail.com>
2017-10-17 10:42:45 -0400

    corrected allergy and diet labels & added search parameters

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const RECIPE_SEARCH_URL = 'https://api.edamam.com/search';
 
 let qVal;
 let numIng;
+let calRange;
 let dietLabel;
 let allergyLab;
-let calRange;
 
-function getDataFromApi(searchTerm, callback, numIng, dietLabel, allergyLab, calRange) {
+
+function getDataFromApi(searchTerm, callback, numIng, calRange, dietLabel, allergyLab) {
   const settings = {
     url: RECIPE_SEARCH_URL,
     data: {
@@ -14,7 +15,7 @@ function getDataFromApi(searchTerm, callback, numIng, dietLabel, allergyLab, cal
       app_key: 'ec9336245111d37ee12a4e9ae1777690',
       app_id: 'b3f870bd',
       from: 0,
-      to: 20,
+      to: 100,
       ingr: numIng,
       diet: dietLabel,
       health: allergyLab,
@@ -35,11 +36,34 @@ function displayRecipeSearchData(data) {
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
+    // Get recipe or ingredient search term
     const queryTarget = $(event.currentTarget).find('.js-query');
     qVal = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    getDataFromApi(qVal, displayRecipeSearchData);
+    // Get the number of ingredients
+    ingTarget = $(event.currentTarget).find('.numIng');
+    numIng = ingTarget.val();
+    ingTarget.val("");
+    // Get the calorie range values
+    calLowTarget = $(event.currentTarget).find('.low');
+    calHiTarget = $(event.currentTarget).find('.high');
+    let low = calLowTarget.val();
+    calLowTarget.val("");
+    let high = calHiTarget.val();
+    calHiTarget.val("");
+    calRange = `gte ${low}, lte ${high}`;
+    // Get the diet labels that are checked
+    dietTarget = $('input[name=diet]:checked', '.js-search-form');
+    dietLabel = dietTarget.val();
+    dietTarget.attr('checked',false);
+    // Get the allergy label checked
+    allTarget = $('input[name=allergy]:checked', '.js-search-form');
+    allergyLab = allTarget.val();
+    console.log(allergyLab);
+    $('input[name=allergy]', '.js-search-form').attr('checked',false);
+    // Call function to get data from API
+    getDataFromApi(qVal, displayRecipeSearchData, numIng, calRange, dietLabel, allergyLab);
   });
 }
 
