e598119645af7db4ce9315a322253774067fa5fd
Sarah Ann Garcia <garciatres@gmail.com>
2017-10-17 13:10:21 -0400

    multiple diet & allergy labels & missing advanced search terms

--- app.js
+++ app.js
@@ -15,16 +15,28 @@ function getDataFromApi(searchTerm, callback, numIng, calRange, dietLabel, aller
       app_key: 'ec9336245111d37ee12a4e9ae1777690',
       app_id: 'b3f870bd',
       from: 0,
-      to: 100,
-      ingr: numIng,
-      diet: dietLabel,
-      health: allergyLab,
-      calories: calRange
+      to: 100
     },
     dataType: 'json',
     type: 'GET',
     success: callback
   };
+
+  if (numIng != "") {
+    settings.data.ingr = numIng;
+  }
+
+  if (dietLabel != "") {
+    settings.data.diet = dietLabel;
+  }
+
+  if (allergyLab.length != 0) {
+    settings.data.health = allergyLab;
+  }
+
+  if (calRange != "") {
+    settings.data.calories = calRange;
+  }
 
   $.ajax(settings);
 }
@@ -40,28 +52,44 @@ function watchSubmit() {
     const queryTarget = $(event.currentTarget).find('.js-query');
     qVal = queryTarget.val();
     // clear out the input
-    queryTarget.val("");
+    // queryTarget.val("");
     // Get the number of ingredients
     ingTarget = $(event.currentTarget).find('.numIng');
     numIng = ingTarget.val();
-    ingTarget.val("");
+    // ingTarget.val("");
     // Get the calorie range values
     calLowTarget = $(event.currentTarget).find('.low');
     calHiTarget = $(event.currentTarget).find('.high');
     let low = calLowTarget.val();
-    calLowTarget.val("");
+    // calLowTarget.val("");
     let high = calHiTarget.val();
-    calHiTarget.val("");
-    calRange = `gte ${low}, lte ${high}`;
+    // calHiTarget.val("");
+
+    if (low != "" && high != "") {
+      calRange = `gte ${low}, lte ${high}`;
+    }
+    else if (high != "") {
+      calRange = `lte ${high}`;
+    }
+    else if (low != ""){
+      calRange = `gte ${low}`;
+    }
+    else {
+      calRange = "";
+    }
+
     // Get the diet labels that are checked
     dietTarget = $('input[name=diet]:checked', '.js-search-form');
     dietLabel = dietTarget.val();
-    dietTarget.attr('checked',false);
+    // dietTarget.attr('checked',false);
     // Get the allergy label checked
     allTarget = $('input[name=allergy]:checked', '.js-search-form');
-    allergyLab = allTarget.val();
+    console.log("something here", allTarget);
+    allergyLab = allTarget.map((index, target) => target.value).toArray();
     console.log(allergyLab);
-    $('input[name=allergy]', '.js-search-form').attr('checked',false);
+    // $('input[name=allergy]', '.js-search-form').attr('checked',false);
+    $('.js-search-form')[0].reset();
+
     // Call function to get data from API
     getDataFromApi(qVal, displayRecipeSearchData, numIng, calRange, dietLabel, allergyLab);
   });
